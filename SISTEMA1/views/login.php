09f47ab85202fa3ddba7351ff732636a3e2655c0
imluisanchez <ls9528352@gmail.com>
2021-02-16 01:29:33 -0600

    LOGO OTAC Y FONDO CON IMAGEN DILUSIDA.

--- SISTEMA1/views/login.php
+++ SISTEMA1/views/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Variedades Otac</title>
     <link rel="stylesheet" href="views/css/login.css">
     <link rel="stylesheet" href="views/css/cabecera.css">
     <link rel="stylesheet" href="">
@@ -13,7 +13,8 @@
 		 <?php
 		 include("../controllers/validar.php")
 		 ?>
-   <h1 class="animate__animated animate__backInLeft">Sistema de login</h1>
+     <img src = 'otac3.jpg' width="200" height="180" >
+   <h1 class="animate__animated animate__backInLeft">Inicio de sesion</h1>
    <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario" style="text-transform:uppercase;" value=""onkeyup="javascript:this.value=this.value.toUpperCase();"></p>
    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
    <input type="submit" value="Ingresar">
