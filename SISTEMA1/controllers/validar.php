13a15045f52b0e8bd936345a5074213c97cd0534
DayanaF <dayana.fernandez@unah.hn>
2021-02-16 00:38:34 -0400

    validar

--- SISTEMA1/controllers/validar.php
+++ SISTEMA1/controllers/validar.php
@@ -5,23 +5,21 @@ $contraseña=$_POST['contraseña'];
 session_start();
 $_SESSION['usuario']=$usuario;
 
+//conectar a la base de datos
 $conexion=mysqli_connect("localhost","root","","db_variedadesotac");
 
-$consulta="SELECT*FROM TBL_MS_USUARIOS where usuario='$usuario' and contraseña='$contraseña'";
+$consulta="SELECT*FROM tbl_ms_usuario where usuario='$usuario' and contraseña='$contraseña'";
 $resultado=mysqli_query($conexion,$consulta);
 
 $filas=mysqli_num_rows($resultado);
 
 
-if($filas){
+if($filas > 0){
 
     header("location:../views/home.php");
 
 }else{
-    ?>
-    <?php
-    include("index.php");
-
+  echo "error en la autenticacion";
 
 }
 mysqli_free_result($resultado);
--- SISTEMA1/views/login.php
+++ SISTEMA1/views/login.php
@@ -8,61 +8,6 @@
     <link rel="stylesheet" href="views/css/cabecera.css">
     <link rel="stylesheet" href="">
 </head>
-<<<<<<< HEAD
-<?php
-$usuario=$_POST['usuario'];
-$contraseña=$_POST['contraseña'];
-session_start();
-$_SESSION['usuario']=$usuario;
-
-$conexion=mysqli_connect("localhost","root","","db_variedadesotac");
-
-$consulta="SELECT * FROM TBL_MS_USUARIOS where usuario='$usuario' and contraseña='$contraseña'";
-$resultado=mysqli_query($conexion,$consulta);
-
-$filas=mysqli_fetch_array($resultado);
-
-if($filas['TBL_MS_ROLES_id_rol']==1){ //administrador
-    header("location:admin.php");
-
-}else
-if($filas['TBL_MS_ROLES_id_rol']==2){ //vendedor
-header("location:cliente.php");
-}
-else{
-    ?>
-    <?php
-    include("index.html");
-    ?>
-    <h1 class="bad">ERROR EN LA AUTENTIFICACION</h1>
-    <?php
-}
-mysqli_free_result($resultado);
-mysqli_close($conexion);
-<body>
-	<div class="login-wrap cover">
-		<div class="container-login">
-			<p class="text-center" style="font-size: 80px;">
-				<img src="imagenes/otac.jpg" width"150" height="150">
-			</p>
-			<center><font color="green" face="candara">INGRESE SUS DATOS</font></center>
-			<form action="views/home.php" method= "POST">
-				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-					<input class="mdl-textfield__input" type="text" id="userName">
-					<label class="mdl-textfield__label" for="userName">Usuario</label>
-				</div>
-				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-					<input class="mdl-textfield__input" type="password" id="pass">
-					<label class="mdl-textfield__label" for="pass">Contraseña</label>
-				</div>
-				<button class="mdl-button mdl-js-button mdl-js-ripple-effect"  style="color: #3F51B5; margin: 0 auto; display: block;">
-					Iniciar Sesion
-				</button>
-			</form>
-		</div>
-	</div>
-</body>
-=======
 <body>
    <form action="controllers/validar.php" method="post">
 		 <?php
@@ -72,7 +17,6 @@ mysqli_close($conexion);
    <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
    <input type="submit" value="Ingresar">
->>>>>>> 3a9078f2c800c7c0d28dfc721385a8d8d970babe
 
    </form>
 
