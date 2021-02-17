507eccf226579849d2ad979b31d6bb22fc332b3a
fjcs2103 <cabreraf83@gmail.com>
2021-02-17 14:30:01 -0600

    COMENTARIOS

--- SISTEMA1/controllers/validar.php
+++ SISTEMA1/controllers/validar.php
@@ -3,34 +3,35 @@
 
 include('../model/conexion.php'); //
 
-$usuario=$_POST['usuario'];
-$contraseña=$_POST['contraseña'];
+$usuario=$_POST['usuario'];           //Variable Usuario que almacenara los datos del input Usuario
+$contraseña=$_POST['contraseña'];     //Variable Contraseña que almacenara los datos del input Contra
 
 
 session_start();
 $_SESSION['usuario']=$usuario;
 
 //conectar a la base de datos
-$conexion=mysqli_connect("localhost","root","","db_variedadesotac");
+$conexion=mysqli_connect("localhost","root","","db_variedadesotac");    //Jalamos los datos de la BD
 
+    //Realizamos un SELECT para Validar la BD con los valores almmacenados en las varibales
 $consulta="SELECT*FROM tbl_ms_usuario where usuario='$usuario' and contraseña='$contraseña'";
 $resultado=mysqli_query($conexion,$consulta); //donde se guarda la conexion y la consulta a la base de datos
 
 $filas=mysqli_num_rows($resultado);
 
-if ($usuario=='' or $contraseña==''){
-echo "<script>alert('LLENAR CAMPOS VACIOS');</script>";
-}else {
-if($filas > 0){
+  if ($usuario=='' or $contraseña==''){                 //Validacion si los campos estan vacios tirar una alerta
+  echo "<script>alert('LLENAR CAMPOS VACIOS');</script>";
+  }else {
+  if($filas > 0){             //Si los campos de usuario y contraseña estan registrados en la BD acceder a la pantalla home
 
-    header("location:../views/home.php");
+      header("location:../views/home.php");
 
-}else{
-  echo "<script>alert('ERROR EN LA AUTENTICACION');</script>";
+  }else{                      //Si los datos no son correctos tirar una alerta
+    echo "<script>alert('ERROR EN LA AUTENTICACION');</script>";
 
-}
+  }
 
-}
+  }
 mysqli_free_result($resultado);
 mysqli_close($conexion);
 ?>
--- SISTEMA1/views/login.php
+++ SISTEMA1/views/login.php
@@ -9,13 +9,15 @@
     <link rel="stylesheet" href="">
 </head>
 <body>
-   <form action="controllers/validar.php" method="post">
+   <form action="controllers/validar.php" method="post"> //METODOS POST para la conexion de BD
 		 <?php
 		 include("../controllers/validar.php")
 		 ?>
      <img src = 'otac3.jpg' width="200" height="180" >
    <h1 class="animate__animated animate__backInLeft">Inicio de sesion</h1>
+              //El nombre del input debe ser usuario ya que lo mandamos a llamar con la conexión mediante el metodo POST
    <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario" style="text-transform:uppercase;" value=""onkeyup="javascript:this.value=this.value.toUpperCase();"></p>
+              //El nombre del input debe ser contraseña ya que lo mandamos a llamar con la conexión mediante el metodo POST
    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
    <input type="submit" value="Ingresar">
 
